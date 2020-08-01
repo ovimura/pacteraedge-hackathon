90e191d892ec64d47c09d09982b9f1cab3b3f6c1
Ovidiu Mura <mura@pdx.edu>
2020-07-31 19:31:34 -0700

    adding script to run daily scrapper daily

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,12 +1,14 @@
 
 # Define Time for Scheduler
-$Time=New-ScheduledTaskTrigger -At 4.00AM -Daily -DaysInterval 1
+$Time=New-ScheduledTaskTrigger -At 4AM -Daily -DaysInterval 1
 
 # Set Actions to be Performed During Execution
 
-$Action=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory C:/repos/pacteraedge-hackathon/pacteraedge-hackathon/scripts -Argument “C:\repos\pacteraedge-hackathon\pacteraedge-hackathon\scripts\main.ps1 -UserName omura -Password pass
+$Action=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory "C:/repos/pacteraedge-hackathon/pacteraedge-hackathon/scripts" -Argument "C:\repos\pacteraedge-hackathon\pacteraedge-hackathon\scripts\main.ps1 -UserName omura -Password pass"
 
 
 # Save Scheduler:
 
 Register-ScheduledTask -TaskName "Run Web Scraper Daily" -Trigger $Time -Action $Action -RunLevel Highest
+
+
