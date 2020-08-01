69011345b0aa200a6ac7ed52404a14028b1fcbab
Ovidiu Mura <mura@pdx.edu>
2020-07-31 19:38:28 -0700

    adding powershell to run on the daily morning on my local for java artifact

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -4,11 +4,11 @@ $Time=New-ScheduledTaskTrigger -At 4AM -Daily -DaysInterval 1
 
 # Set Actions to be Performed During Execution
 
-$Action=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory "C:/repos/pacteraedge-hackathon/pacteraedge-hackathon/scripts" -Argument "C:\repos\pacteraedge-hackathon\pacteraedge-hackathon\scripts\main.ps1 -UserName omura -Password pass"
+$Action=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory "C:/repos/pacteraedge-hackathon/pacteraedge-hackathon/target" -Argument "java -jar web-scraper-0.0.1-SNAPSHOT.jar -UserName omura -Password pass"
 
 
 # Save Scheduler:
 
-Register-ScheduledTask -TaskName "Run Web Scraper Daily" -Trigger $Time -Action $Action -RunLevel Highest
+Register-ScheduledTask -TaskName "Run Java Web Scraper Daily" -Trigger $Time -Action $Action -RunLevel Highest
 
 
