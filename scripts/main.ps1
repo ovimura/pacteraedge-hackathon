c8b8f2bf8bdc727ef2795bafd434526516d57940
Ovidiu Mura <mura@pdx.edu>
2020-07-31 19:20:24 -0700

    add powershell script to trigger the daily web scraper every day in the morning at 4:00AM

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,1 +1,12 @@
 
+# Define Time for Scheduler
+$Time=New-ScheduledTaskTrigger -At 4.00AM -Daily -DaysInterval 1
+
+# Set Actions to be Performed During Execution
+
+$Action=New-ScheduledTaskAction -Execute PowerShell.exe -WorkingDirectory C:/repos/pacteraedge-hackathon/pacteraedge-hackathon/scripts -Argument “C:\repos\pacteraedge-hackathon\pacteraedge-hackathon\scripts\main.ps1 -UserName omura -Password pass
+
+
+# Save Scheduler:
+
+Register-ScheduledTask -TaskName "Run Web Scraper Daily" -Trigger $Time -Action $Action -RunLevel Highest
